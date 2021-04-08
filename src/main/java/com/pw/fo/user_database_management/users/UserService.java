cd32b19ca583b8df507aa77645a1bc2141af0386
Paulina Anna Wierzchowska <pwierzchowska.lublin@gmail.com>
2021-04-08 15:38:20 +0200

    Create User Service

---
+++ src/main/java/com/pw/fo/user_database_management/users/UserService.java
@@ -0,0 +1,12 @@
+package com.pw.fo.user_database_management.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+}
