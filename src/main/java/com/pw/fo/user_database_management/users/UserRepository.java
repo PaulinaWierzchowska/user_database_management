80c1620441789184fadf6d65fd79156b61072d10
Paulina Anna Wierzchowska <pwierzchowska.lublin@gmail.com>
2021-04-08 15:34:44 +0200

    Create User Repository

---
+++ src/main/java/com/pw/fo/user_database_management/users/UserRepository.java
@@ -0,0 +1,7 @@
+package com.pw.fo.user_database_management.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
