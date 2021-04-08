3f7011621abb76ee69ed1513af11eabb180fe4d8
Paulina Anna Wierzchowska <pwierzchowska.lublin@gmail.com>
2021-04-08 15:06:10 +0200

    Initial commit

---
+++ src/main/java/com/pw/fo/user_database_management/UserDatabaseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.pw.fo.user_database_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDatabaseManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserDatabaseManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pw/fo/user_database_management/UserDatabaseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pw.fo.user_database_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDatabaseManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
